e19432777751b4ee18801f3fdbadbf7c04414048
augustlewen <augustlewen@live.com>
2024-11-06 11:46:08 +0100

    added reach destination check

--- Goblin King/Assets/Scripts/GameSystems/Units/AI/AINavMovement.cs
+++ Goblin King/Assets/Scripts/GameSystems/Units/AI/AINavMovement.cs
@@ -9,23 +9,47 @@ namespace GameSystems.Units.AI
         
         private AIGrid grid; // Reference to the grid
         private Vector2Int gridPosition; // The current grid position
-
+        private bool hasDestination;
+        
+        
         private void Start()
         {
             agent = GetComponent<NavMeshAgent>();
             agent.updateRotation = false; // Prevents rotation in 2D
             agent.updateUpAxis = false; // Ensures the agent stays in 2D plane
         }
+        
+        public virtual void Update()
+        {
+            if (hasDestination &&  HasReachedDestination())
+            {
+                OnReachDestination();
+            }
+        }
+
+        public virtual void OnReachDestination()
+        {
+            hasDestination = false;
+        }
 
         protected void SetDestination(Vector2 pos)
         {
+            hasDestination = true;
             NavMeshPath path = new NavMeshPath();
             agent.CalculatePath(pos, path);
 
             if (path.status == NavMeshPathStatus.PathComplete)
                 agent.SetDestination(pos);
-            else
-                Debug.LogWarning("Destination not reachable");
+        }
+        
+        private bool HasReachedDestination()
+        {
+            // Return true if the agent has a destination, is not calculating a path,
+            // and remainingDistance is very close to zero
+            return !agent.pathPending && 
+                   agent.remainingDistance <= agent.stoppingDistance && 
+                   !agent.hasPath && 
+                   !agent.isStopped;
         }
     }
 }
