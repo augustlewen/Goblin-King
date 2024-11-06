d7e3ac6fe8c43b928d4822a2a0900d671b33a5ef
augustlewen <augustlewen@live.com>
2024-11-06 10:41:34 +0100

    Update BreakableObject.cs

--- Goblin King/Assets/Scripts/GameSystems/InteractableObjects/BreakableObject.cs
+++ Goblin King/Assets/Scripts/GameSystems/InteractableObjects/BreakableObject.cs
@@ -1,14 +1,19 @@
 using GameSystems.Items;
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace GameSystems.InteractableObjects
 {
-    public class BreakableObject : MonoBehaviour
+    public class BreakableObject : MonoBehaviour, ISelect
     {
-        public ItemSO_Tool.ToolType itemInteraction;
+        public ItemSO_Tool.ToolType toolRequired;
         public ItemSO dropItem;
         public int hp;
+        public void OnSelect()
+        {
+            
+        }
 
         public void TakeDamage(int damage)
         {
@@ -27,5 +32,6 @@ namespace GameSystems.InteractableObjects
             
             Destroy(gameObject);
         }
+
     }
 }
