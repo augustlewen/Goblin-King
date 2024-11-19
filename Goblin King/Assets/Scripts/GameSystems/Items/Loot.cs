59121006ef82157d908a9b603c77101cd6d78ef1
augustlewen <augustlewen@live.com>
2024-11-19 10:36:21 +0100

    removed debug logs

--- Goblin King/Assets/Scripts/GameSystems/Items/Loot.cs
+++ Goblin King/Assets/Scripts/GameSystems/Items/Loot.cs
@@ -28,11 +28,8 @@ namespace GameSystems.Items
                 if (bagInventory.AddItem(itemData))
                 {
                     items.Remove(itemData);
-                    Debug.Log("LOOTING : " + itemData.itemSO.itemName);
                 }
             }
-            Debug.Log("Items Left To Loot: " + items.Count);
-
             if(items.Count == 0)
                 Destroy(gameObject);
         }
