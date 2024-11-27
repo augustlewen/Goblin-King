398a3cb0c362a7ceb68c90d96cd17c8747ac23d2
augustlewen <augustlewen@live.com>
2024-11-27 09:58:24 +0100

    added enemy stats

---
+++ Goblin King/Assets/Scripts/GameSystems/Units/Enemies/EnemyStats.cs
@@ -0,0 +1,39 @@
+using System;
+using GameSystems.Items;
+using GameSystems.Items.SO;
+
+namespace GameSystems.Units.Enemies
+{
+    public class EnemyStats : UnitStats
+    {
+        public ItemSO startItem;
+        public LootTable lootTable;
+        
+        private void Start()
+        {
+            if(startItem != null)
+                EquipItem(startItem.CreateItemData());
+        }
+
+        protected override void OnDeath()
+        {
+            lootTable.DropItems(transform.position);
+            base.OnDeath();
+        }
+
+        public void EquipItem(ItemData item)
+        {
+            equippedItem = item;
+
+            if (item == null)
+                return;
+
+            if (equippedItem.GetItemType() != ItemType.Weapon) 
+                return;
+            
+            var weapon = item.GetSpecificData<ItemWeaponData>().weaponSO;
+            GetComponent<CombatAIBehaviour>().UpdateStats(weapon.power, weapon.range, weapon.haste);
+        }
+
+    }
+}
