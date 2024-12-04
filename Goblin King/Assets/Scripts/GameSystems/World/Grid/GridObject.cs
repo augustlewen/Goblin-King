dd18226529f51deebf1c017e961d1a17ee3ba6e3
augustlewen <augustlewen@live.com>
2024-12-04 12:54:30 +0100

    Update GridObject.cs

--- Goblin King/Assets/Scripts/GameSystems/World/Grid/GridObject.cs
+++ Goblin King/Assets/Scripts/GameSystems/World/Grid/GridObject.cs
@@ -1,20 +1,25 @@
 using System;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace GameSystems.World.Grid
 {
     public class GridObject : MonoBehaviour
     {
+        public UnityEvent<GridObjectSO> OnGridObjectSetup = new();
+        
         private string goName;
         private Vector2Int gridSize;
         
-        public virtual void Setup(GridObjectSO gridObjectSO)
+        public void Setup(GridObjectSO gridObjectSO)
         {
             goName = gridObjectSO.gosoName;
             gridSize = gridObjectSO.gridSize;
             gameObject.name = gridObjectSO.gosoName;
             
             GetComponent<SpriteRenderer>().sprite = gridObjectSO.sprite;
+            
+            OnGridObjectSetup.Invoke(gridObjectSO);
         }
 
         private void OnDisable()
